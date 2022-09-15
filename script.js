b2cac6006ef56cecf1468505483e741673f87ad5
Akash <veisthebest3@gmail.com>
2022-09-15 19:24:21 +0530

    Made basic UI and some fixes. Will revisit in future after I get more experienced.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 let count, row, column;
-count = prompt('Add value');
-canvas = document.querySelector('.canvas');
-for (row = 1; row <= count; row++) {
-    let rPixel = document.createElement('div')
-    rPixel.classList.add('pixel');
-    canvas.appendChild(rPixel);
+count = prompt('Pick a value from 16-100');
+if (count < 16 || count > 100) {
+    count = prompt('Invalid choice.Pick a value between 16-100.');
+}
+else {
+    canvas = document.querySelector('.canvas');
+    for (row = 1; row <= count; row++) {
+        let rPixel = document.createElement('div')
+        rPixel.classList.add('pixel');
+        canvas.appendChild(rPixel);
+
+        for (column = 1; column <= count; column++) {
+            let cPixel = document.createElement('div');
+            cPixel.classList.add('pixel');
+            rPixel.appendChild(cPixel);
+
+            cPixel.addEventListener('mousemove', (e) => {
+                e.target.classList.add('ChangeColor');
+            })
+
+        }
 
 
-    for (column = 1; column <= count; column++) {
-        let cPixel = document.createElement('div');
-        cPixel.classList.add('pixel');
-        rPixel.appendChild(cPixel);
-
-        cPixel.addEventListener('mousemove', (e) => {
-            e.target.classList.add('ChangeColor');
-        })
 
     }
 
-
-
 }
-
-
